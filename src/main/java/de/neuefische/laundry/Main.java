74ec963cdcecd5cacad82052367513a84e10b5a2
barbarahellmann <babshellmann@web.de>
2024-03-14 22:05:59 +0100

    Added equals and hashCode

--- src/main/java/de/neuefische/laundry/Main.java
+++ src/main/java/de/neuefische/laundry/Main.java
@@ -1,9 +1,12 @@
 package de.neuefische.laundry;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         WashingMachine washingMachine = new WashingMachine();
         washingMachine.fill("T-Shirt");
+        System.out.println(washingMachine);
         washingMachine.turnOn();
         washingMachine.waitUntilFinish();
         String result = washingMachine.empty();
--- src/main/java/de/neuefische/laundry/WashingMachine.java
+++ src/main/java/de/neuefische/laundry/WashingMachine.java
@@ -1,16 +1,19 @@
 package de.neuefische.laundry;
 
+import java.util.Objects;
+
 public class WashingMachine {
     private String contents = "";
     private boolean clean = true;
-    public boolean getClean() {
-        return clean;
-    }
 
-    public void setClean(boolean clean) {
-        this.clean = clean;
-    }
-
+//    public boolean getClean() {
+//        return clean;
+//    }
+////
+//    public void setClean(boolean clean) {
+//        this.clean = clean;
+//    }
+//
     public void fill(String item) {
         contents += item + ", ";
         clean = false;
@@ -38,5 +41,28 @@ public class WashingMachine {
         result += contents.substring(0, contents.length() - 2);
         contents = "";
         return result;
+    }
+
+    public String toString() {
+        String text = "Eine Waschmaschine, sie ist ";
+        if (clean) {
+            text += "Sauber";
+        } else {
+            text += "schmutzig";
+        }
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WashingMachine that = (WashingMachine) o;
+        return clean == that.clean && Objects.equals(contents, that.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contents, clean);
     }
 }
